fb3c769b8d8b77dd89f78efa8469e2f54b4c4cd6
Noluyolo97 <98300986+Noluyolo97@users.noreply.github.com>
2022-02-08 13:56:41 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,44 @@
-#include "-putchar"
-/**
- * print_times_table  - give you the multiplication table conditinated by n
- * @n: input that configure the table
- */
+#include "_putchar"
 void print_times_table(int n)
 {
-	int x, y, z, uni, dec, cen;
-
-	if (n <= 15 && n >= 0)
-	{
-		for (x = 0; x <= n; x++)
-		{
-			for (y = 0; y <= n; y++)
-			{
-				z = x * y;
-				cen = z / 100;
-				dec = (z / 10);
-				dec = dec % 10;
-				uni = z % 10;
-				if ((y > 0) && (n > 0))
-				{ _putchar(' ');
-					if (!(cen >= 1))
-						_putchar(' ');
-					if (cen >= 1)
-						_putchar('0' + cen);
-					if (!(dec >= 1) && !(cen >= 1))
-						_putchar(' ');
-					if (!(dec >= 1) && (cen >= 1))
-						_putchar('0' + dec);
-					if (dec >= 1)
-						_putchar('0' + dec);
-					_putchar('0' + uni);
-					if (!(y == n))
-						_putchar(','); }
-				else if ((y == 0) && (n > 0))
-				{ _putchar('0' + z);
-					_putchar(','); }
-				else if ((y == 0) && (n == 0))
-					_putchar('0'); }
-			_putchar('\n'); } }
+  int i;
+  int j;
+​
+  if (n <= 15 && n >= 0)
+  {
+  for (i = 0; i <= n; i++)
+  {
+  for (j = 0; j <= n; j++)
+  {
+  int prod = j * i;
+​
+  if (j == 0)
+  {
+     _putchar('0');
+  } else if (prod <= 9)
+  {
+     _putchar(',');
+     _putchar(' ');
+     _putchar(' ');
+     _putchar(' ');
+     _putchar('0' + prod);
+   } else if (prod > 99)
+   {
+     _putchar(',');
+     _putchar(' ');
+     _putchar('0' + (prod / 100));
+     _putchar('0' + ((prod / 10) % 10));
+     _putchar('0' + (prod % 10));
+   } else
+   {
+     _putchar(',');
+     _putchar(' ');
+     _putchar(' ');
+     _putchar('0' + (prod / 10));
+     _putchar('0' + (prod % 10));
+   }
+   }
+     _putchar('\n');
+   }
+   }
 }
