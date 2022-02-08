96e5ce373f53f01564fc35ba661a54c9560f028c
Noluyolo97 <98300986+Noluyolo97@users.noreply.github.com>
2022-02-08 14:48:36 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,47 @@
 #include "main.h"
 /**
- * print_times_table  - give you the multiplication table conditinated by n
- * @n: input that configure the table
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: Nothing.
  */
 void print_times_table(int n)
 {
-	int x, y, z, uni, dec, cen;
+	int i, j, res;
 
-	if (n <= 15 && n >= 0)
+	if (!(n > 15 || n < 0))
 	{
-		for (x = 0; x <= n; x++)
+		for (i = 0; i <= n; i++)
 		{
-			for (y = 0; y <= n; y++)
+			for (j = 0; j <= n; j++)
 			{
-				z = x * y;
-				cen = z / 100;
-				dec = (z / 10);
-				dec = dec % 10;
-				uni = z % 10;
-				if ((y > 0) && (n > 0))
-				{ _putchar(' ');
-					if (!(cen >= 1))
-						_putchar(' ');
-					if (cen >= 1)
-						_putchar('0' + cen);
-					if (!(dec >= 1) && !(cen >= 1))
-						_putchar(' ');
-					if (!(dec >= 1) && (cen >= 1))
-						_putchar('0' + dec);
-					if (dec >= 1)
-						_putchar('0' + dec);
-					_putchar('0' + uni);
-					if (!(y == n))
-						_putchar(','); }
-				else if ((y == 0) && (n > 0))
-				{ _putchar('0' + z);
-					_putchar(','); }
-				else if ((y == 0) && (n == 0))
-					_putchar('0'); }
-			_putchar('\n'); } }
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
