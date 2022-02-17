251a05f1d3930afe1d9ff937d4b17285a6f2aa88
Noluyolo97 <98300986+Noluyolo97@users.noreply.github.com>
2022-02-17 15:01:54 +0200

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,5 +8,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-char *infinite_add(char *n1, char *n2, char *n3, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *n3, int size_r);
 void print_number(int n);
