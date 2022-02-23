34c68e4f5ad1d32af537ae349f358bc00d570d2d
Noluyolo97 <98300986+Noluyolo97@users.noreply.github.com>
2022-02-23 09:13:11 +0200

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,68 +1,111 @@
 #include "main.h"
 
 /**
- * move_past_star - iterates past asterisk
- * @s2: the second string, can contain wildcard
- *
- * Return: the pointer past star
+ * prueba - test if are equal if not return c or b
+ * @a: string one
+ * @b: string two
+ * @c: string that save the original b
+ * Return: a value or go to match
  */
-char *move_past_star(char *s2)
+
+int prueba(char *a, char *b, char *c)
 {
-	if (*s2 == '*')
-		return (move_past_star(s2 + 1));
+	if (a[0] == '\0' && b[0] == '\0')
+	{
+		return (1);
+	}
 	else
-		return (s2);
+	{
+		if (a[0] == b[0])
+		{
+			a++;
+			b++;
+			return (prueba(a, b, c));
+		}
+		else
+		{
+			if (b[0] == '*')
+			{
+				return (match(a, b));
+			}
+			else
+			{
+				return (match(a, c));
+			}
+		}
+	}
 }
-
 /**
- * inception - makes magic a reality
- * @s1: the first string
- * @s2: the second string, can contain wildcard
- *
- * Return: 1 if identical, 0 if false
+ * match - compare char by char if are the same go to prueba
+ * @a: string one
+ * @b: string two
+ * Return: value 1 or 0
  */
-int inception(char *s1, char *s2)
+int  match(char *a, char *b)
 {
-	int ret = 0;
-
-	if (*s1 == 0)
-		return (0);
-	if (*s1 == *s2)
-		ret += wildcmp(s1 + 1, s2 + 1);
-	ret += inception(s1 + 1, s2);
-	return (ret);
+	if (b[0] == '*')
+	{
+		b++;
+		return (match(a, b));
+	}
+	else
+	{
+		if (a[0] != b[0])
+		{
+			if (a[0] == '\0')
+			{
+				return (0);
+			}
+			else
+			{
+				a++;
+				return (match(a, b));
+			}
+		}
+		else
+		{
+			return (prueba(a, b, b));
+		}
+	}
 }
-
 /**
- * wildcmp - compares two strings lexicographically
- * @s1: the first string
- * @s2: the second string, can contain wildcard
- *
- * Return: 1 if identical, 0 if false
+ * wildcmp - prints if both strings are considered identical
+ * @s1: string one
+ * @s2: string two
+ * Return: 1 if are considered identical, 0 if not
  */
 int wildcmp(char *s1, char *s2)
 {
-	int ret = 0;
-
-	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
-		return (1);
-	if (*s1 == *s2)
+	if (s2[0] == '*')
 	{
-		if (!*s1)
-			return (1);
-		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+		if (s2[1] == '*')
+		{
+			s2++;
+			return (wildcmp(s1, s2));
+		}
+		else
+		{
+			return (match(s1, s2));
+		}
 	}
-	if (!*s1 || !s2)
-		return (0);
-	if (*s2 == '*')
+	else
 	{
-		s2 = move_past_star(s2);
-		if (!*s2)
-			return (1);
-		if (*s1 == *s2)
-			ret += wildcmp(s1 + 1, s2 + 1);
-		ret += inception(s1, s2);
-		return (!!ret);
+		if (s1[0] == s2[0])
+		{
+			if (s1[0] != '\0' && s2[0] != '\0')
+			{
+				s1++;
+				s2++;
+				return (wildcmp(s1, s2));
+			}
+			else
+			{
+				return (1);
+			}
+		}
+		else
+		{
+			return (0);
+		}
 	}
-	return (0);
 }
